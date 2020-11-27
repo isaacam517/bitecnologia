2574eac4cebf24597cb2db60bab2e64864c9a6e9
Isaac Martiniano <isaacam517@gmail.com>
2020-11-27 16:04:33 -0300

    Select com a verifição de no prazo ou fora do prazo

--- listar_projetos_back.php
+++ listar_projetos_back.php
@@ -3,10 +3,18 @@
         //conexao com o banco
         include("conexao_bd.php");
         // Pegar os projetos armazenados no BD:
-        $consulta = $conn->prepare("SELECT * FROM projetos");
+        $consulta = $conn->prepare("SELECT *,
+        CASE WHEN prazo > NOW() THEN
+        '<span class=text-success><strong> No prazo </strong></span>'
+        ELSE 
+        '<span class=text-danger><strong> Fora do prazo </strong></span>'
+        END AS 'prazos'
+        FROM projetos");
         $consulta->execute();
 
         $result_lista_proj = $consulta->fetchAll();
+       
+
         } catch(PDOException $e) {
             echo "Conexão falhou: " . $e->getMessage();
         }
